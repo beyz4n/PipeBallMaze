860888f8f04f62c936fea048d76aa91f0c8c3665
beyz4n <beyzanurkayaxs@gmail.com>
2022-04-23 23:53:36 +0300

    add

--- src/Tiles/LinearPipe.java
+++ src/Tiles/LinearPipe.java
@@ -5,9 +5,9 @@ import javafx.scene.image.Image;
 public class LinearPipe extends Tile implements Movable {
     public LinearPipe(String status){
         if(status.equals("Vertical"))
-            setImage(new Image("VerticalPipe.jpg"));
+            setImage(new Image("VerticalPipe v2.png"));
         else if (status.equals("Horizontal"))
-            setImage(new Image("HorizontalPipe.jpg"));
+            setImage(new Image("HorizontalPipe v2.png"));
     }
 
     @Override
