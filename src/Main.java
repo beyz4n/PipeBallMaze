c579ad35d6651373a15e56d68a909f5e04eba2d3
beyz4n <beyzanurkayaxs@gmail.com>
2022-04-23 03:40:28 +0300

    scene için adım attım ama hoşuna giderse tutalım, artı 2 ball seçeneği de ekledim.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Tiles.*;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -20,15 +21,39 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
-/*
+
+        Button startButton = new Button("Start");
+        StackPane startPane = new StackPane();
+        startPane.getChildren().add(startButton);
+        Scene startScene = new Scene(startPane,850,780);
+        Stage startStage = new Stage();
+        startStage.setScene(startScene);
+        startStage.setTitle("PipeBallMaze");
+        startStage.show();
+
+        startButton.setOnMouseClicked(event -> {
+            startStage.hide();
+            primaryStage.show();
+        });
+
+
         File folder = new File("Levels");
+        ArrayList<File> levels = new ArrayList<>();
         for (int l = 0; l < folder.list().length; l++) {
             String levelNo = l + 1 + "";
-            File file = new File("Levels\\level" + levelNo);
+            File file = new File("Levels\\level" + levelNo + ".txt");
+            levels.add(file);
+        }
 
- */
-        File file = new File("input.txt");
-            Scanner input = new Scanner(file);
+        //for (int m = 0; m < levels.size(); m++) { }
+
+            Scanner input = null;
+            try {
+                input = new Scanner(levels.get(1));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
             ArrayList<String> lines = new ArrayList<>();
             while (input.hasNext()) {
                 lines.add(input.next());
@@ -61,22 +86,20 @@ public class Main extends Application {
                 }
 
                 if (words.get(k + 1).equals("Pipe")) {
-                    if (words.get(k+2).equals("Vertical") || words.get(k+2).equals("Horizontal")) {
+                    if (words.get(k + 2).equals("Vertical") || words.get(k + 2).equals("Horizontal")) {
                         tiles[queue / 4][queue % 4] = new LinearPipe(words.get(k + 2));
-                    }
-                    else if(words.get(k+2).equals("00") ||words.get(k+2).equals("01") ||words.get(k+2).equals("10") ||words.get(k+2).equals("11")){
+                    } else if (words.get(k + 2).equals("00") || words.get(k + 2).equals("01") || words.get(k + 2).equals("10") || words.get(k + 2).equals("11")) {
                         tiles[queue / 4][queue % 4] = new CurvedPipeMovable(words.get(k + 2));
                     }
                 }
 
                 if (words.get(k + 1).equals("PipeStatic")) {
-                    if (words.get(k+2).equals("Vertical") || words.get(k+2).equals("Horizontal")) {
+                    if (words.get(k + 2).equals("Vertical") || words.get(k + 2).equals("Horizontal")) {
                         tiles[queue / 4][queue % 4] = new NormalPipeStatic(words.get(k + 2));
-                    }
-                    else if(words.get(k+2).equals("00") ||words.get(k+2).equals("01") ||words.get(k+2).equals("10") ||words.get(k+2).equals("11")){
+                    } else if (words.get(k + 2).equals("00") || words.get(k + 2).equals("01") || words.get(k + 2).equals("10") || words.get(k + 2).equals("11")) {
                         tiles[queue / 4][queue % 4] = new CurvedPipeStatic(words.get(k + 2));
                     }
-                    
+
                 }
 
                 if (words.get(k + 1).equals("End")) {
@@ -87,128 +110,128 @@ public class Main extends Application {
 
             // Pane Layout
 
-        BorderPane borderPane = new BorderPane();
-        Pane pane = new Pane();
-        borderPane.setTop(new EdgePane("Top"));
-        borderPane.setRight(new EdgePane("Right"));
-        borderPane.setBottom(new EdgePane("Bottom"));
-        borderPane.setLeft(new EdgePane("Left"));
-        borderPane.setCenter(pane);
+            BorderPane borderPane = new BorderPane();
+            Pane pane = new Pane();
+            borderPane.setTop(new EdgePane("Top"));
+            borderPane.setRight(new EdgePane("Right"));
+            borderPane.setBottom(new EdgePane("Bottom"));
+            borderPane.setLeft(new EdgePane("Left"));
+            borderPane.setCenter(pane);
 
 
-        ImageView imageView1 = new ImageView(tiles[0][0].getImage());
-        imageView1.setX(0);
-        imageView1.setY(0);
-        imageView1.setFitHeight(175);
-        imageView1.setFitWidth(175);
+            ImageView imageView1 = new ImageView(tiles[0][0].getImage());
+            imageView1.setX(0);
+            imageView1.setY(0);
+            imageView1.setFitHeight(175);
+            imageView1.setFitWidth(175);
 
-        ImageView imageView2 = new ImageView(tiles[0][1].getImage());
-        imageView2.setX(180);
-        imageView2.setY(0);
-        imageView2.setFitHeight(175);
-        imageView2.setFitWidth(175);
+            ImageView imageView2 = new ImageView(tiles[0][1].getImage());
+            imageView2.setX(180);
+            imageView2.setY(0);
+            imageView2.setFitHeight(175);
+            imageView2.setFitWidth(175);
 
-        ImageView imageView3 = new ImageView(tiles[0][2].getImage());
-        imageView3.setX(360);
-        imageView3.setY(0);
-        imageView3.setFitHeight(175);
-        imageView3.setFitWidth(175);
+            ImageView imageView3 = new ImageView(tiles[0][2].getImage());
+            imageView3.setX(360);
+            imageView3.setY(0);
+            imageView3.setFitHeight(175);
+            imageView3.setFitWidth(175);
 
-        ImageView imageView4 = new ImageView(tiles[0][3].getImage());
-        imageView4.setX(540);
-        imageView4.setY(0);
-        imageView4.setFitHeight(175);
-        imageView4.setFitWidth(175);
+            ImageView imageView4 = new ImageView(tiles[0][3].getImage());
+            imageView4.setX(540);
+            imageView4.setY(0);
+            imageView4.setFitHeight(175);
+            imageView4.setFitWidth(175);
 
-        ImageView imageView5 = new ImageView(tiles[1][0].getImage());
-        imageView5.setX(0);
-        imageView5.setY(180);
-        imageView5.setFitHeight(175);
-        imageView5.setFitWidth(175);
+            ImageView imageView5 = new ImageView(tiles[1][0].getImage());
+            imageView5.setX(0);
+            imageView5.setY(180);
+            imageView5.setFitHeight(175);
+            imageView5.setFitWidth(175);
 
-        ImageView imageView6 = new ImageView(tiles[1][1].getImage());
-        imageView6.setX(180);
-        imageView6.setY(180);
-        imageView6.setFitHeight(175);
-        imageView6.setFitWidth(175);
+            ImageView imageView6 = new ImageView(tiles[1][1].getImage());
+            imageView6.setX(180);
+            imageView6.setY(180);
+            imageView6.setFitHeight(175);
+            imageView6.setFitWidth(175);
 
-        ImageView imageView7 = new ImageView(tiles[1][2].getImage());
-        imageView7.setX(360);
-        imageView7.setY(180);
-        imageView7.setFitHeight(175);
-        imageView7.setFitWidth(175);
+            ImageView imageView7 = new ImageView(tiles[1][2].getImage());
+            imageView7.setX(360);
+            imageView7.setY(180);
+            imageView7.setFitHeight(175);
+            imageView7.setFitWidth(175);
 
-        ImageView imageView8 = new ImageView(tiles[1][3].getImage());
-        imageView8.setX(540);
-        imageView8.setY(180);
-        imageView8.setFitHeight(175);
-        imageView8.setFitWidth(175);
+            ImageView imageView8 = new ImageView(tiles[1][3].getImage());
+            imageView8.setX(540);
+            imageView8.setY(180);
+            imageView8.setFitHeight(175);
+            imageView8.setFitWidth(175);
 
-        ImageView imageView9 = new ImageView(tiles[2][0].getImage());
-        imageView9.setX(0);
-        imageView9.setY(360);
-        imageView9.setFitHeight(175);
-        imageView9.setFitWidth(175);
+            ImageView imageView9 = new ImageView(tiles[2][0].getImage());
+            imageView9.setX(0);
+            imageView9.setY(360);
+            imageView9.setFitHeight(175);
+            imageView9.setFitWidth(175);
 
-        ImageView imageView10 = new ImageView(tiles[2][1].getImage());
-        imageView10.setX(180);
-        imageView10.setY(360);
-        imageView10.setFitHeight(175);
-        imageView10.setFitWidth(175);
+            ImageView imageView10 = new ImageView(tiles[2][1].getImage());
+            imageView10.setX(180);
+            imageView10.setY(360);
+            imageView10.setFitHeight(175);
+            imageView10.setFitWidth(175);
 
-        ImageView imageView11 = new ImageView(tiles[2][2].getImage());
-        imageView11.setX(360);
-        imageView11.setY(360);
-        imageView11.setFitHeight(175);
-        imageView11.setFitWidth(175);
+            ImageView imageView11 = new ImageView(tiles[2][2].getImage());
+            imageView11.setX(360);
+            imageView11.setY(360);
+            imageView11.setFitHeight(175);
+            imageView11.setFitWidth(175);
 
-        ImageView imageView12 = new ImageView(tiles[2][3].getImage());
-        imageView12.setX(540);
-        imageView12.setY(360);
-        imageView12.setFitHeight(175);
-        imageView12.setFitWidth(175);
+            ImageView imageView12 = new ImageView(tiles[2][3].getImage());
+            imageView12.setX(540);
+            imageView12.setY(360);
+            imageView12.setFitHeight(175);
+            imageView12.setFitWidth(175);
 
-        ImageView imageView13 = new ImageView(tiles[3][0].getImage());
-        imageView13.setX(0);
-        imageView13.setY(540);
-        imageView13.setFitHeight(175);
-        imageView13.setFitWidth(175);
+            ImageView imageView13 = new ImageView(tiles[3][0].getImage());
+            imageView13.setX(0);
+            imageView13.setY(540);
+            imageView13.setFitHeight(175);
+            imageView13.setFitWidth(175);
 
-        ImageView imageView14 = new ImageView(tiles[3][1].getImage());
-        imageView14.setX(180);
-        imageView14.setY(540);
-        imageView14.setFitHeight(175);
-        imageView14.setFitWidth(175);
+            ImageView imageView14 = new ImageView(tiles[3][1].getImage());
+            imageView14.setX(180);
+            imageView14.setY(540);
+            imageView14.setFitHeight(175);
+            imageView14.setFitWidth(175);
 
-        ImageView imageView15 = new ImageView(tiles[3][2].getImage());
-        imageView15.setX(360);
-        imageView15.setY(540);
-        imageView15.setFitHeight(175);
-        imageView15.setFitWidth(175);
+            ImageView imageView15 = new ImageView(tiles[3][2].getImage());
+            imageView15.setX(360);
+            imageView15.setY(540);
+            imageView15.setFitHeight(175);
+            imageView15.setFitWidth(175);
 
-        ImageView imageView16 = new ImageView(tiles[3][3].getImage());
-        imageView16.setX(540);
-        imageView16.setY(540);
-        imageView16.setFitHeight(175);
-        imageView16.setFitWidth(175);
+            ImageView imageView16 = new ImageView(tiles[3][3].getImage());
+            imageView16.setX(540);
+            imageView16.setY(540);
+            imageView16.setFitHeight(175);
+            imageView16.setFitWidth(175);
 
 
-        pane.getChildren().addAll(imageView1, imageView2, imageView3,imageView4,
-                imageView5, imageView6, imageView7, imageView8, imageView9, imageView10,
-                imageView11, imageView12, imageView13, imageView14, imageView15, imageView16);
+            pane.getChildren().addAll(imageView1, imageView2, imageView3, imageView4,
+                    imageView5, imageView6, imageView7, imageView8, imageView9, imageView10,
+                    imageView11, imageView12, imageView13, imageView14, imageView15, imageView16);
 
-        ImageView[] imageViews =  {imageView1, imageView2, imageView3,imageView4,
-                imageView5, imageView6, imageView7, imageView8, imageView9, imageView10,
-                imageView11, imageView12, imageView13, imageView14, imageView15, imageView16};
+            ImageView[] imageViews = {imageView1, imageView2, imageView3, imageView4,
+                    imageView5, imageView6, imageView7, imageView8, imageView9, imageView10,
+                    imageView11, imageView12, imageView13, imageView14, imageView15, imageView16};
 
-        drag(imageViews, tiles);
+            drag(imageViews, tiles);
 
-        Scene scene = new Scene(borderPane, 930,850);
-        primaryStage.setTitle("PipeBallMaze");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            Scene scene = new Scene(borderPane, 930, 850);
+            primaryStage.setTitle("PipeBallMaze");
+            primaryStage.setScene(scene);
+            //primaryStage.show();
 
-        }
+}
 
     private void drag(ImageView[] imageViews, Tile[][] tiles) {
         for (int i = 0; i < imageViews.length; i++){
