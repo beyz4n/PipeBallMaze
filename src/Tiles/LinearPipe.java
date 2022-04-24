12cfeb98ad792b8be6be9105cdbc3150afd61bcd
beyz4n <beyzanurkayaxs@gmail.com>
2022-04-24 12:53:47 +0300

    Pipe commit

--- src/Tiles/LinearPipe.java
+++ src/Tiles/LinearPipe.java
@@ -4,10 +4,15 @@ import javafx.scene.image.Image;
 
 public class LinearPipe extends Tile implements Movable {
     public LinearPipe(String status){
-        if(status.equals("Vertical"))
+        if(status.equals("Vertical")) {
             setImage(new Image("VerticalPipe.png"));
-        else if (status.equals("Horizontal"))
+            setStatus(status);
+        }
+        else if (status.equals("Horizontal")){
             setImage(new Image("HorizontalPipe.png"));
+            setStatus(status);
+        }
+
     }
 
     @Override
--- src/Tiles/NormalPipeStatic.java
+++ src/Tiles/NormalPipeStatic.java
@@ -4,9 +4,13 @@ import javafx.scene.image.Image;
 
 public class NormalPipeStatic extends PipeStatic implements Fixed {
     public NormalPipeStatic (String status){
-        if(status.equals("Vertical"))
+        if(status.equals("Vertical")) {
             setImage(new Image("VerticalPipeStatic.png"));
-        else if(status.equals("Horizontal"))
+            setStatus(status);
+        }
+        else if(status.equals("Horizontal")) {
             setImage(new Image("HorizontalPipeStatic.png"));
+            setStatus(status);
+        }
     }
 }
